904d8530b47c9381727dd31e4987a8844c946573
mizuruRL <andrecfdias42@gmail.com>
2022-11-15 14:22:47 +0000

    initial sqrt parsing logic

--- YAPCalc/util/parser.py
+++ YAPCalc/util/parser.py
@@ -23,17 +23,34 @@ class Parser:
             raise TypeError
     
     def prep_expression(self, expression):
-        pexpression = expression.translate({ord(c): None for c in string.whitespace})
-        if "%" in pexpression:
-            tokens = r"([+*/%-])"
+        if "%" in expression:
+            pexpression = self.clean_expr(expression)
+            tokens = r"([%]|(sqrt))"
             tokexpr = re.split(tokens, pexpression)
+            
             for i in range(len(tokexpr) - 1):
                 if tokexpr[i] == "%" and self.is_float(tokexpr[i+1]):
                     print(tokexpr[i])
                     tokexpr[i+1] = str(float(tokexpr[i+1]) * 0.01)
                     tokexpr.pop(i)
+                
+            
             pexpression = ''.join(tokexpr)
         return pexpression
+
+    def calc_sqrt(self, tokexpr):
+        if "sqrt" in tokexpr:
+            sqrt_index = tokexpr.index("sqrt")
+            joined_expr = ''.join(tokexpr[sqrt_index + 1])
+            res = self.calc_expression(joined_expr)
+            tokexpr[sqrt_index + 1] = res
+            tokexpr.pop(sqrt_index)
+            
+            
+    
+    def clean_expr(self, expr):
+        return expr.translate({ord(c): None for c in string.whitespace})
+
 
     def is_float(self, str):
         try:
